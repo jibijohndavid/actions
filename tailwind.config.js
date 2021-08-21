5332a371b8c2bb77aabcc2e16647de6968da5a3c
Jibi John David <jibijohndavid@gmail.com>
2021-08-22 01:36:22 +0530

    test load dot-dev source

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                'dark': '#16161e',
+            },
+            backgroundImage: theme => ({
+                'header-img': "url('/images/gradient-bg.svg')",
+            }),
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
